a5ea3662d603f2bf78d2b05231b6a98e09de8541
McConderEz <kamazitun.jaykehd@gmail.com>
2025-02-18 11:31:31 +0300

    fixes

--- src/WhoCame.Web/Program.cs
+++ src/WhoCame.Web/Program.cs
@@ -1,12 +1,24 @@
+using WhoCame.Framework.Middlewares;
 using WhoCame.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddLogger(builder.Configuration);
 builder.Services.AddSwagger();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionMiddleware();
+
+app.MapControllers();
 
 app.Run();
